5fe9f273be7aadd0430f835d879ecbbcbfe66d87
sstavh <sashatalabirshuk@gmail.com>
2025-12-23 20:11:41 +0200

    end test

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,15 @@
 module.exports = {
+  projectId: 'o69kf5',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
     },
   },
 };
